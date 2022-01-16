a077c81f01abba9022ad595664a29b60d8465da5
Seoin <68271159+tjdls111@users.noreply.github.com>
2022-01-16 16:00:49 +0900

    Add card maker

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,11 +1,42 @@
 import React from "react";
+import Button from "../button/button";
 import styles from "./card_edit_form.module.css";
+import ImgInput from "../img_input/img_input.jsx";
 
 function CardEditForm({ card }) {
+  const DEFAULT_IMAGE = "/images/defaultlogo.png";
+  const { id, name, company, theme, job, email, motto, imgName, imgURL } = card;
+  const url = imgURL || DEFAULT_IMAGE;
+
+  const onSubmit = () => {};
   return (
-    <>
-      <h1>{card.name}</h1>
-    </>
+    <form className={styles.form}>
+      <input type="text" className={styles.text} name="name" value={name} />
+      <input
+        type="text"
+        className={styles.text}
+        name="company"
+        value={company}
+      />
+      <input type="text" className={styles.text} name="job" value={job} />
+      <input type="text" className={styles.text} name="email" value={email} />
+      <textarea
+        className={styles.textarea}
+        name="motto"
+        cols="30"
+        rows="5"
+        value={motto}
+      />
+      <select className={styles.select} name="theme" value={theme}>
+        <option value="dark">dark</option>
+        <option value="light">light</option>
+        <option value="pink">pink</option>
+      </select>
+      <div className={styles.fileInput}>
+        <Button className={styles.button} name="delete" onClick={onSubmit} />
+        <ImgInput className={styles.inputBtn} />
+      </div>
+    </form>
   );
 }
 
---
+++ src/components/img_input/img_input.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./img_input.module.css";
+
+function ImgInput() {
+  return <button>Image</button>;
+}
+
+export default ImgInput;
